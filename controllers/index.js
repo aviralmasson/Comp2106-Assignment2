39aadf8a97c28d87385604177071f83eddb44e68
aviralmasson <31753079+aviralmasson@users.noreply.github.com>
2018-04-02 02:17:57 -0400

    Web pages created

--- controllers/index.js
+++ controllers/index.js
@@ -11,31 +11,31 @@ router.get('/', function(req, res, next) {
 router.get('/pageone', function(req, res, next) {
     res.render('pageone', {
         title: 'Classified',
-        message: 'Shop Sedan'
+        message: ''
     });
 });
 router.get('/pagetwo', function(req, res, next) {
     res.render('pagetwo', {
-        title: 'Classified',
-        message: 'Shop Hatchback'
+        title: 'HatchBack',
+        message: ''
     });
 });
 router.get('/pagethree', function(req, res, next) {
     res.render('pagethree', {
-        title: 'Classified',
+        title: 'SUV',
         message: 'Shop SUV'
     });
 });
 router.get('/pagefour', function(req, res, next) {
     res.render('pagefour', {
-        title: 'Classified',
-        message: 'Shop Convertible'
+        title: 'Convertible',
+        message: ''
     });
 });
 router.get('/pagefive', function(req, res, next) {
     res.render('pagefive', {
-        title: 'Classified',
-        message: 'Shop Sportscar'
+        title: 'Sports',
+        message: ''
     });
 });
 
